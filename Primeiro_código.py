71515285a098a7c0ecfe67d40d2b54b9863cab74
KKMF122 <shark08337@sharkcoders.pt>
2025-09-27 10:31:39 +0100

    Add files via upload

---
+++ Primeiro_código.py
@@ -0,0 +1,24 @@
+n1 = 5
+n2 = 10
+
+soma = n1 + n2
+
+print("A soma dá:",soma)
+
+
+n3 = 6
+n4 = 12
+
+multiplicação = n3 * n4
+print("A multiplicação dá:",multiplicação)
+
+
+n6 = 6
+n7 = 2
+
+divisão = n6 / n7
+print("A divisão dá:",divisão)
+
+
+média = (n1 + n2 + n3 + n4 + n6 + n7) / 6
+print("A média dá:",média)
